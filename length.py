af946c0d6c9cde00ccc9dfa6f5536b0644079ef6
shashankhegde06 <141432130+shashankhegde06@users.noreply.github.com>
2023-11-05 12:55:30 +0530

    Create length.py

---
+++ length.py
@@ -0,0 +1,14 @@
+def count_letters(input_string):
+    letter_count = 0
+    
+    for char in input_string:
+        if char.isalpha():
+            letter_count += 1
+            
+    return letter_count
+
+# Example Usage
+input_string = "Hello World! How are you?"
+result = count_letters(input_string)
+
+print(f'The number of letters in the string is: {result}')
