cf9b3d115625572d392ca4c36e232f2d98516a75
shashankhegde06 <141432130+shashankhegde06@users.noreply.github.com>
2023-11-06 00:04:17 +0530

    Update try2.py

--- try2.py
+++ try2.py
@@ -1,8 +1,10 @@
+import os
+
 def factorial(n):
     result = 1
     for i in range(1, n + 1):
         result *= i
     return result
 
-num = int(input("Enter a number: "))
+num = int(os.popen('read -p "Enter a number: " var ; echo $var').read())
 print(f"The factorial of {num} is {factorial(num)}")
